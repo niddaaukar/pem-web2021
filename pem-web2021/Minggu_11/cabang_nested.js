5cbbd5656ca7bac3bdff944ba98d2d1090114d6c
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-17 00:08:35 +0700

    update minggu 11

---
+++ pem-web2021/Minggu_11/cabang_nested.js
@@ -0,0 +1,11 @@
+    var username = prompt("Username:");
+    var password = prompt("Password:");
+         if(username == "chikaLia12"){
+         if(password == "cantiK21"){
+         document.write("<h2>Selamat datang saudari Chika!</h2>");
+         } else {
+         document.write("<p>Password salah, coba lagi!</p>");
+         }
+         } else {
+         document.write("<p>Anda tidak terdaftar!</p>");
+         }
