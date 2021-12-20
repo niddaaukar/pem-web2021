6d4470e1d0d124d917b87bb13de90e0ed40dee2d
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-20 10:17:26 +0700

    update

--- pem-web2021/Minggu_11/perulangan_for.js
+++ pem-web2021/Minggu_11/perulangan_for.js
@@ -2,6 +2,6 @@
 //hitungan akan berhenti saat kondisi i>0 
 //i-- merupakan simbol untuk pengurangan yang nantinya program (mengurangi 1)
 for(let i = 10; i > 0; i--){
-    document.write("<p>Perulangan ke-" - i - "</p>")
+    document.write(i + "  ")
    }
    
