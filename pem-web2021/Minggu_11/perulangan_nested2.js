c60a75b820221e31c030ff620e572c45cae9f422
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-20 10:55:37 +0700

    Update perulangan_nested2.js

--- pem-web2021/Minggu_11/perulangan_nested2.js
+++ pem-web2021/Minggu_11/perulangan_nested2.js
@@ -3,6 +3,6 @@ var counter = 0;
 while (ulangi) {
  counter++;
  var bintang = "*".repeat(counter) + "<br>";
- document.write(counter + ": " + bintang);
+ document.write(counter + " : " + bintang);
  ulangi = confirm("apakah anda ingin mengulang?");
 }
