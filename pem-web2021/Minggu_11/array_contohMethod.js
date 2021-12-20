39a650dd8e3fe927d306524b96fe0dde766b4e80
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-20 18:05:36 +0700

    Update array_contohMethod.js

--- pem-web2021/Minggu_11/array_contohMethod.js
+++ pem-web2021/Minggu_11/array_contohMethod.js
@@ -2,7 +2,7 @@
 const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // ambil data yang hanya habis dibagi dua 
 const filteredArray = angka.filter((item) => item % 2 === 0);
-document.write(<p> filteredArray</p> + "<br>");// -> [2, 4, 6, 8]
+document.write(filteredArray + "<br>");// -> [2, 4, 6, 8]
 // Method Includes
 var tanaman = ["Padi", "Kacang", "Jagung", "Kedelai"];
 var adaKacang = tanaman.includes("Kacang");
@@ -13,5 +13,5 @@ document.write(adaBayam + "<br>"); // -> false
 //Method Short
 var alfabet = ['a','f','z','e','r','g'];
 document.write(alfabet.sort() + "<br>"); //-> ["a", "e", "f", "g", "r", "z"] mengurutkan alfabet
-var angka = [3,1,2,6,8,5];
-document.write(angka.sort() + "<br>"); // -> [1, 2, 3, 4, 5, 6, 7, 8, 9] mengurutkan angka
+var angka2 = [3,1,2,6,8,5];
+document.write(angka2.sort() + "<br>"); // -> [1, 2, 3, 4, 5, 6, 7, 8, 9] mengurutkan angka
