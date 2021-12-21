35091565d249a852296668d632fe729740f6eefc
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-21 13:48:00 +0700

    Update confirm.js

--- pem-web2021/Minggu_10/Tugas/confirm.js
+++ pem-web2021/Minggu_10/Tugas/confirm.js
@@ -1,6 +1,6 @@
 var tanya = confirm("Apakah kamu yakin akan mengunjungi akun github saya?");
  if ( tanya == true){
-      window.location = "http://dinus.ac.id ";
+      window.location = "https://github.com/niddaaukar/pem-web2021";
      } else {
      window.location = "belum, tetap di sini saja";
      }
