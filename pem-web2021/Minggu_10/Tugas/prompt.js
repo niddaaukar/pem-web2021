3aa0dfafd29ce339b301bfef386539a7cfd5d266
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-21 13:58:36 +0700

    Update prompt.js

--- pem-web2021/Minggu_10/Tugas/prompt.js
+++ pem-web2021/Minggu_10/Tugas/prompt.js
@@ -1,5 +1,5 @@
 var nama = prompt("Siapa nama kamu?", "");
- document.write("<h1>Hello "+ nama +"diwebsite ku</h1>");
+ document.write("<h1>Hello "+ nama + "  Selamat datang di restoran kami </h1> ");
 
  var makananFavorit = prompt("Apa makanan favorit anda?");
  document.write("<h2><br>Makanan Favoritmu "+ makananFavorit +"</h2>");
