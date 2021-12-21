76aa2bedf8db44a3e98e94af66e89e78e05bfb62
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-21 13:53:51 +0700

    Update op_ternary.js

--- pem-web2021/Minggu_10/Tugas/op_ternary.js
+++ pem-web2021/Minggu_10/Tugas/op_ternary.js
@@ -1,3 +1,3 @@
 var pertanyaan = confirm ("sudah mengerjakan tugas pbw minggu 10?");
-var jawab = pertanyaan ? " sudah dong " : prompt ("aduh, belum nih..");
+var jawab = pertanyaan ? " sudah dong " : "aduh, belum nih..";
 document.write(jawab);
