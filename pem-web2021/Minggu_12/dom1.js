06122cc7a2c64583ff2a68bfcb4deec5d0bf173b
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-23 11:45:41 +0700

    update dom1

---
+++ pem-web2021/Minggu_12/dom1.js
@@ -0,0 +1,21 @@
+// mengakses elemen tutorial
+var tutorial = document.getElementById("tutorial");
+// mengisi teks ke dalam elemen
+tutorial.innerText = "Tutorial Javascript";
+// memberikan CSS ke elemen
+tutorial.style.backgroundColor = "gold";
+tutorial.style.padding = "15px";
+tutorial.style.textAlign = "center";
+tutorial.style.fontSize = "30px";
+//tutorial2
+var tutorial2 = document.getElementById("tutorial2");
+tutorial2.innerHTML = "Perkenalkan Nama saya NIDA AULIA KARIMA";
+tutorial2.style.backgroundColor = "#008B8B";
+tutorial2.style.color = "#fff";
+tutorial2.style.padding = "15px";
+tutorial2.style.fontSize = "13px";
+tutorial2.style.textAlign = "center";
+tutorial2.style.border = "30px";
+
+
+
