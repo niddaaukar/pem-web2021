3366250cab852b36af9993853eb304e887ceabf5
niddaauliak <91897864+niddaaukar@users.noreply.github.com>
2021-12-27 09:29:32 +0700

    update

---
+++ pem-web2021/Minggu_12/js_objek/objek_1.js
@@ -0,0 +1,11 @@
+var person = {
+    firstName: "Nida Aulia Karima",
+    lastName: "Karima",
+    //method
+    showName: function(){
+    alert(`Nama: ${this.firstName} ${this.lastName}`);
+    }
+    };
+    // panggil method
+    person.showName();
+   
